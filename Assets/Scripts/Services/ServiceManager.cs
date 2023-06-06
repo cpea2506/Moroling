a675fea9fccbb5105b0b40099a6559882b5b5fd1
CPea <cpea2506@gmail.com>
2023-06-07 01:56:33 +0700

    Remove unecessary service

--- Assets/Scripts/Services/ServiceManager.cs
+++ Assets/Scripts/Services/ServiceManager.cs
@@ -4,11 +4,13 @@ public class ServiceManager : MonoBehaviour
 {
     public static Service service { get; } = new Service();
 
-    // Start is called before the first frame update
     private void Awake()
     {
         service.Register(new PlayerInputAction());
-        service.Register(new GameState());
-        service.Register(new Dice());
+    }
+
+    private void OnEnable()
+    {
+        service.Get<PlayerInputAction>().Enable();
     }
 }
