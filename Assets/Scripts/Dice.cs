b766bc7478c8a3b8d536e691ec28b02cb64b48f8
CPea <cpea2506@gmail.com>
2023-06-07 02:04:27 +0700

    Return the value of dice after tossing

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -1,33 +1,33 @@
+using Cinemachine;
 using UnityEngine;
 
 public class Dice : MonoBehaviour
 {
-    private int currentNumber;
+    public static int currentNumber;
 
     private Rigidbody diceRigidbody;
-    private Vector3 diceVelocity;
 
-    // Start is called before the first frame update
+    [SerializeField]
+    private CinemachineVirtualCamera followCamera;
+
     private void Start()
     {
         diceRigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     private void Update()
     {
-        diceVelocity = diceRigidbody.velocity;
-
-        // if (ServiceManager.service.Get<PlayerInputAction>().Player.RollDice.IsPressed())
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded)
+        if (
+            ServiceManager.service.Get<PlayerInputAction>().Player.TossingDice.IsPressed()
+            && IsGrounded
+        )
         {
-            Debug.Log("I'm rolling");
             diceRigidbody.useGravity = true;
 
             float x = Random.Range(0f, 500f);
             float y = Random.Range(0f, 500f);
             float z = Random.Range(0f, 500f);
-            transform.position = new Vector3(0, 0.2f, 0);
+            transform.position = new Vector3(0, 2, 0);
             transform.rotation = Quaternion.identity;
             diceRigidbody.AddForce(transform.up * 500);
             diceRigidbody.AddTorque(x, y, z);
